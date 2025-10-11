2ae3d060e36457ca4fc6640d6dec5fb7ad627101
Bruno Kendy <kendy.iuata@gmail.com>
2025-10-10 21:00:40 -0300

    increments and operators

--- hello.java
+++ hello.java
@@ -2,13 +2,23 @@ class hello
 {
     public static void main(String args[])
     {
-        byte a = 10;
-        byte b = 30;
+        int num = 7;
+        //int num2 = 5;
 
-        int result = a * b;
+        //int result = num1 % num2;
+        //num = num + 2;
+        //num += 1;
+        //num++;            // post-increment
+        //num--;            // pre-increment
+        //++num;
+
+        int result = num++; //fetch the value and then increment
+        //int result = ++num //increment and then fetch the value
 
         System.out.println(result);
 
+
+        
     }
         
 }
