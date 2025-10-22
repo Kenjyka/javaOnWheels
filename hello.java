2a06885d71ed31dd35659ab6db985d82ec7c1c93
Bruno Kendy <kendy.iuata@gmail.com>
2025-10-22 20:31:40 -0300

    ternary operator

--- hello.java
+++ hello.java
@@ -3,25 +3,21 @@ class hello
     public static void main(String args[])
     {
 
-        int x = 8;
-        int y = 7;
-        int z = 9;
+       int n = 4;
+       int result = 0;
 
-     
-        if (x > y && x > z) 
-        {
-            System.out.println(x);
-        }
-        else if (y > z)
-        {
-            System.out.println(y);
-        } 
-        else 
-        {
-            System.out.println(z)
-        }
 
-     
+       /*if (n % 2 == 0) {
+            result = 10;
+       }
+       else {
+        result = 20;
+       }*/
+
+       result = n % 2 == 0 ? 10 : 20;
+
+        System.out.println(result);
+
 
     }
         
