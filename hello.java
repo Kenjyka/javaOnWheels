934e64d8d4771116ceb4722f222f4a5b82ddc983
Bruno Kendy <kendy.iuata@gmail.com>
2025-10-22 20:40:39 -0300

    switch case

--- hello.java
+++ hello.java
@@ -3,21 +3,35 @@ class hello
     public static void main(String args[])
     {
 
-       int n = 4;
-       int result = 0;
+        int n = 1
 
+        Swwitch (n) {
+            case 1:
+                System.out.println("Monday");
+                break;
+            case 2:
+                System.out.println("Tuesday");
+                break;
+            case 3:
+                System.out.println("Wednesday");
+                break;
+            case 4:
+                System.out.println("Thursday");
+                break;
+            case 5:
+                System.out.println("Friday");
+                break;
+            case 6:
+                System.out.println("Saturday");
+                break;
+            case 7:
+                System.out.println("Sunday");        
+                break;
+            default:
+                System.out.println("Enter a valid number")
+        }
 
-       /*if (n % 2 == 0) {
-            result = 10;
-       }
-       else {
-        result = 20;
-       }*/
-
-       result = n % 2 == 0 ? 10 : 20;
-
-        System.out.println(result);
-
+    
 
     }
         
